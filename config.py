ca10563a91d140f226a13df2df17cc2192a0f237
JoeJiJa <168629159+JoeJiJa@users.noreply.github.com>
2025-03-04 16:07:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://adscomjai:3GcLpN9JbD3Bi8WB@cluster0.t8owj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "samplezztestbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
